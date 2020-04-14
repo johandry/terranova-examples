026a2fc75a49e2b13be5d47b4a80df85fb4966ef
Johandry Amador <johandry@gmail.com>
2020-04-14 12:57:17 -0700

    Using Export feature from Terranova

--- 06_go-templates/main.go
+++ 06_go-templates/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"os"
 )
 
 func main() {
@@ -13,33 +11,32 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 
-	if opt.Export {
-		if err := ioutil.WriteFile("main.tf", []byte(tfCode), 0644); err != nil {
-			log.Fatalf("failed to export the Terraform code to 'main.tf'. %v", err)
-		}
-		return
+	switch {
+	case opt.Export:
+		log.Printf("Starting export of Terraform code to %s", exportDir)
+	case opt.Destroy:
+		log.Printf("Starting deprovisioning of the LetsChat server on AWS")
+	default:
+		log.Printf("Starting provisioning of the LetsChat server on AWS")
 	}
-
-	log := log.New(os.Stderr, "", log.LstdFlags)
-	log.Printf("Starting (de)provisioning of the LetsChat server on AWS")
 
 	publicIP, err := apply(tfCode, opt)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if opt.Destroy {
-		log.Printf("The web server has been destroyed")
-		return
-	}
-
-	if opt.Status {
-		log.Printf("Check the status and logs at: http://%s:%s", publicIP, opt.StatusPort)
-	}
-
-	log.Printf("The web server will be ready at: http://%s:%s", publicIP, opt.Port)
-
-	if opt.SSHAccess {
-		log.Printf("Connect to the server executing: ssh -i %s %s@%s", opt.PrivKeyFile, "ubuntu", publicIP)
+	switch {
+	case opt.Export:
+		log.Printf("The Terraform code has been exported to %s", exportDir)
+	case opt.Destroy:
+		log.Printf("The LetsChat server has been destroyed")
+	default:
+		log.Printf("The LetsChat server will be ready at: http://%s:%s", publicIP, opt.Port)
+		if opt.Status {
+			log.Printf("Check the status and logs at: http://%s:%s", publicIP, opt.StatusPort)
+		}
+		if opt.SSHAccess {
+			log.Printf("Connect to the server executing: ssh -i %s %s@%s", opt.PrivKeyFile, "ubuntu", publicIP)
+		}
 	}
 }
--- 06_go-templates/terranova.go
+++ 06_go-templates/terranova.go
@@ -7,27 +7,40 @@ import (
 	"os"
 
 	"github.com/johandry/terranova"
+	"github.com/johandry/terranova-examples/06_go-templates/letschat/data"
 	"github.com/johandry/terranova/logger"
 	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
 
-const stateFilename = "aws-webserver.tfstate"
+const (
+	exportDir     = "./terraform_code"
+	stateFilename = "aws-webserver.tfstate"
+)
 
 func apply(tfCode string, opt *options) (string, error) {
 	logMiddleware := getLogMiddleware(opt)
 	defer logMiddleware.Close()
 
 	platform := terranova.NewPlatform(tfCode).
+		AddFile("user_data.sh", data.UserdataTmpl).
+		AddFile("docker-compose.yaml", data.DockerComposeTmpl).
 		SetMiddleware(logMiddleware).
 		AddProvider("aws", aws.Provider())
-
-	if _, err := platform.PersistStateToFile(stateFilename); err != nil {
-		return "", fmt.Errorf("Fail to create the platform using state file %s. %s", stateFilename, err)
-	}
 
 	if opt.Status {
 		platform.Var("public_key_file", opt.PubKeyFile).
 			Var("private_key_file", opt.PrivKeyFile)
+	}
+
+	if opt.Export {
+		if err := os.MkdirAll(exportDir, 0755); err != nil {
+			return "", err
+		}
+		return "", platform.Export(exportDir)
+	}
+
+	if _, err := platform.PersistStateToFile(stateFilename); err != nil {
+		return "", fmt.Errorf("Fail to create the platform using state file %s. %s", stateFilename, err)
 	}
 
 	if err := platform.Apply(opt.Destroy); err != nil {
